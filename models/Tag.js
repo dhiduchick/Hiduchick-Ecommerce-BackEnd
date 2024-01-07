2d9b4268892a5d0cab7f5d27ac0610982b07bdb8
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-01-07 13:45:14 -0500

    defined column on tags.js

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      authoIncrement: true
+    },
+    tage_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
