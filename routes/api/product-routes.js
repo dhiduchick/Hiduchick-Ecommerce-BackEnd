7bd21b7af75e279af3587c0c09fa88c58c2ffbc8
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-01-10 17:24:39 -0500

    changes made to try and fix product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,10 +3,8 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, {model: Tag, through: ProductTag, as: 'product_tags'}]
